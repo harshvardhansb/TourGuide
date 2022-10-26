e03644ac9b605f50e212f8bc559327f9ba9098f9
rudrakshkarpe <rudraksh.karpe@gmail.com>
2022-10-26 11:59:19 +0530

    Remove unessential .exe file + format documents

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
+const toggle = document.getElementById("toggleDark");
+const body = document.querySelector("body");
+const backtotop = document.getElementById("Bottom");
 
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
-const backtotop = document.getElementById('Bottom');
-
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background = 'white';
-        body.style.color = 'black';
-        backtotop.style.color="black";
-        body.style.transition = '2s';
-        toggle.style.color = 'black';
-    }else{
-        body.style.background = '#1d212b';
-        body.style.color = 'white';
-        body.style.transition = '2s';
-        toggle.style.color = 'black';
-
-    }
+toggle.addEventListener("click", function () {
+  this.classList.toggle("bi-moon");
+  if (this.classList.toggle("bi-brightness-high-fill")) {
+    body.style.background = "white";
+    body.style.color = "black";
+    backtotop.style.color = "black";
+    body.style.transition = "2s";
+    toggle.style.color = "black";
+  } else {
+    body.style.background = "#1d212b";
+    body.style.color = "white";
+    body.style.transition = "2s";
+    toggle.style.color = "black";
+  }
 });
